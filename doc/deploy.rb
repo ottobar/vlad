741cf6d4587ad230dc793082eb908bab32690d20
Don Barlow <ottobar@gmail.com>
2008-12-17 15:14:04 -0500

    added example deploy files

--- doc/deploy.rb
+++ doc/deploy.rb
@@ -1,14 +1,21 @@
-# Vlad configuration for a simple deployment setup
-#
 # For a new deployment:
-# - rake vlad:setup
-# - rake vlad:upload_config FILES=database.yml
-# - rake vlad:update
-# - rake vlad:migrate
-# - rake vlad:start:first_time
+# - $ rake vlad:setup
+# - $ rake vlad:upload_config FILES=database.yml
+# - $ rake vlad:update
+# - $ rake vlad:db:create
+# - $ rake vlad:db:migrate 
+# - $ rake vlad:db:seed 
+# - $ rake vlad:start_app:first_time
+# - configure the web server(s) as needed
+# - $ rake vlad:start_web:first_time
 #
 # For redeploys:
-# - rake vlad:deploy
+# - $ rake vlad:deploy
 #   (runs vlad:stop, vlad:update, vlad:start, vlad:cleanup)
-# - rake vlad:deploy:migrations
-#   (runs vlad:stop, vlad:update, vlad:migrate, vlad:start, vlad:cleanup)
+# - $ rake vlad:deploy:migrations
+#   (runs vlad:stop, vlad:update, vlad:db:migrate, vlad:start, vlad:cleanup)
+#
+set :application, "some-application"
+set :code_repo, "ssh://some.domain.com/path/to/#{application}.git"
+set :deploy_to, "/path/to/#{application}"
+set :domain, 'some.domain.com'
--- doc/deploy_multistage.rb
+++ doc/deploy_multistage.rb
@@ -1,14 +1,31 @@
-# Vlad configuration for a multistage deployment scenario.
-#
 # For a new deployment:
-# - rake [environment] vlad:setup
-# - rake [environment] vlad:upload_config FILES=database.yml
-# - rake [environment] vlad:update
-# - rake [environment] vlad:migrate
-# - rake [environment] vlad:start:first_time
+# - $ rake [environment] vlad:setup
+# - $ rake [environment] vlad:upload_config FILES=database.yml
+# - $ rake [environment] vlad:update
+# - $ rake [environment] vlad:db:create
+# - $ rake [environment] vlad:db:migrate
+# - $ rake [environment] vlad:db:seed 
+# - $ rake [environment] vlad:start_app:first_time
+# - configure the web server(s) as needed
+# - $ rake vlad:start_web:first_time
 #
 # For redeploys:
-# - rake [environment] vlad:deploy
+# - $ rake [environment] vlad:deploy
 #   (runs vlad:stop, vlad:update, vlad:start, vlad:cleanup)
-# - rake [environment] vlad:deploy:migrations
-#   (runs vlad:stop, vlad:update, vlad:migrate, vlad:start, vlad:cleanup)
+# - $ rake [environment] vlad:deploy:migrations
+#   (runs vlad:stop, vlad:update, vlad:db:migrate, vlad:start, vlad:cleanup)
+#
+set :application, "some-application"
+set :code_repo, "ssh://some.domain.com/path/to/#{application}.git"
+
+task :staging do
+  set :deploy_to, "/path/to/#{application}"
+  set :domain, 'some.domain.com'
+  set :app_env, 'staging'
+end
+
+task :production do
+  set :deploy_to, "/path/to/#{application}"
+  set :domain, 'some.domain.com'
+  set :app_env, 'production'
+end
