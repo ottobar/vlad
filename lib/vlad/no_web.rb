07b4dbcc12bfa8b9d3c032f6807988178afb15fb
Don Barlow <ottobar@gmail.com>
2008-12-17 13:12:54 -0500

    added missing task in no_web

--- lib/vlad/no_web.rb
+++ lib/vlad/no_web.rb
@@ -1,8 +1,8 @@
 require 'vlad'
 
-# :web should define vlad:start_web and vlad:stop_web tasks
+# :web should define vlad:start_web, vlad:stop_web and vlad:start_web:first_time tasks
 namespace :vlad do
-  desc "Restart the web and app servers"
+  desc "Restart the web servers"
   remote_task :start_web, :roles => :web do
     puts "Nothing to do to restart the web server"
   end
@@ -10,6 +10,13 @@ namespace :vlad do
   desc "Stop the web servers"
   remote_task :stop_web, :roles => :web do
     puts "Nothing to do to stop the web server"
+  end
+
+  namespace :start_web do
+    desc "Start the web servers for the first time"
+    remote_task :first_time, :roles => :web do
+      puts "Nothing to do to start the web server for the first time"
+    end
   end
   
 end
