8744396673dec61f2d3b3419b200477e78fd4e59
Don Barlow <ottobar@gmail.com>
2008-12-17 14:47:45 -0500

    changed god commands back to run sudo

--- lib/vlad/god.rb
+++ lib/vlad/god.rb
@@ -5,16 +5,16 @@ namespace :vlad do
   set :app_env, 'production'
   
   remote_task :stop_app, :roles => :app do
-    sudo "/usr/bin/god stop #{application}"
+    run "sudo /usr/bin/god stop #{application}"
   end
   remote_task :start_app, :roles => :app do
-    sudo "/usr/bin/god restart #{application}"
+    run "sudo /usr/bin/god restart #{application}"
   end
 
   namespace :start_app do
     desc "start the applicaiton for the first time"
     remote_task :first_time, :role => :app do
-      sudo "/usr/bin/god start #{application} -c #{current_path}/config/#{app_env}.god"
+      run "sudo /usr/bin/god start #{application} -c #{current_path}/config/#{app_env}.god"
     end
   end
 
