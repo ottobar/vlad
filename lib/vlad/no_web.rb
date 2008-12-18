0958b1ac4363f1f381366c7bf22c1732504d2f19
Matt Lyon <matt@flowerpowered.com>
2008-12-17 23:06:28 -0800

    don't require a host set if there is no host to require

--- lib/vlad/no_web.rb
+++ lib/vlad/no_web.rb
@@ -3,19 +3,19 @@ require 'vlad'
 # :web should define vlad:start_web, vlad:stop_web and vlad:start_web:first_time tasks
 namespace :vlad do
   desc "Restart the web servers"
-  remote_task :start_web, :roles => :web do
-    puts "Nothing to do to restart the web server"
+  remote_task :start_web, :roles => nil do
+    # puts "Nothing to do to restart the web server"
   end
 
   desc "Stop the web servers"
-  remote_task :stop_web, :roles => :web do
-    puts "Nothing to do to stop the web server"
+  remote_task :stop_web, :roles => nil do
+    # puts "Nothing to do to stop the web server"
   end
 
   namespace :start_web do
     desc "Start the web servers for the first time"
-    remote_task :first_time, :roles => :web do
-      puts "Nothing to do to start the web server for the first time"
+    remote_task :first_time, :roles => nil do
+      # puts "Nothing to do to start the web server for the first time"
     end
   end
   
