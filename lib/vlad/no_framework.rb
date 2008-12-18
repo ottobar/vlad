84762a564e2d5e7d5527fb1a4b254fd112661feb
Don Barlow <ottobar@gmail.com>
2008-12-18 09:26:48 -0500

    set no_framework roles to nil to match no_app and no_web

--- lib/vlad/no_framework.rb
+++ lib/vlad/no_framework.rb
@@ -5,23 +5,23 @@ namespace :vlad do
   
   namespace :db do
     desc "Create the application database"
-    remote_task :create, :roles => :app do
+    remote_task :create, :roles => nil do
       puts "Nothing to do to create the database"
     end
 
     desc "Migrate the database to the latest version"
-    remote_task :migrate, :roles => :app do
+    remote_task :migrate, :roles => nil do
       puts "Nothing to do to migrate the database"
     end
 
     desc "Load seed data into application database"
-    remote_task :seed, :roles => :app do
+    remote_task :seed, :roles => nil do
       puts "Nothing to do to seed the database"
     end
   end
 
   desc "Updates the framework configuration and working directories after a new release has been exported"
-  remote_task :update_framework, :roles => :app do
+  remote_task :update_framework, :roles => nil do
     puts "Nothing to do to update the framework"
   end
   
