619235eef871a6fdbea60df48f76d2253ba39d79
Don Barlow <ottobar@gmail.com>
2009-01-21 22:06:08 -0500

    removed start and stop tasks from maintenance

--- lib/vlad/maintenance.rb
+++ lib/vlad/maintenance.rb
@@ -9,15 +9,4 @@ namespace :vlad do
     run "cp -f #{shared_path}/config/maintenance.html #{shared_path}/system/"
   end
   
-  desc "(Re)Start the web and app servers"
-  remote_task :start do
-    Rake::Task['vlad:start_app'].invoke
-    Rake::Task['vlad:start_web'].invoke
-  end
-
-  desc "Stop the web and app servers"
-  remote_task :stop do
-    Rake::Task['vlad:stop_app'].invoke
-    Rake::Task['vlad:stop_web'].invoke
-  end
 end
